ab3e8bdbdef3d4c61fd94a7721f79a24c20dcfd8
Jyotiprakash <jp.sahoo@samsung.com>
2015-04-16 10:54:49 +0530

    some sorting algorithms; quicker_sort blitzes past everyone :)

--- src/sort.c
+++ src/sort.c
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /*
  * algorithm is to take each element
@@ -82,6 +83,11 @@ void bubble_sort(int A[], int n)
    }
 }
 
+/* algorithm is to chose a pivot element and partition
+ * other elements in comparision to it and recursively
+ * apply to all partitons until only one element remains.
+ * NOTE: choice of pivot determines the efficiency
+ */
 void quick_sort(int A[], int lo, int hi)
 {
    int P,p,i,t;
@@ -114,20 +120,81 @@ void quick_sort(int A[], int lo, int hi)
    }
 }
 
+/* almost quick sort, only that for smaller ranges of
+ * elements, an insertion sort is used instead of
+ * recursing further.
+ */
+void quicker_sort(int A[], int lo, int hi)
+{
+   int P,p,i,t;
+   if ((hi - lo) == 10)
+   {
+      insertion_sort(A, hi - lo);
+   }   
+   else if (lo < hi)
+   {
+      /* partition the array into two sub array */
+      P = hi; /* choose pivot as last index */
+      p = lo; /* choose an index variable to calculate position of pivot */
+      /* start moving elements based on comparision with pivot */
+      for(i = lo; i <= hi; i++)
+      {
+         if (A[i] < A[P])
+         {
+            if (i != p)
+            {
+               t = A[i];
+               A[i] = A[p];
+               A[p] = t;
+            }
+            p++;
+         }
+      }
+      /* bring pivot element to its right place */
+      t = A[P];
+      A[P] = A[p];
+      A[p] = t;
+      /* recursive calls on subarrays */
+      quicker_sort(A, lo, p - 1);
+      quicker_sort(A, p + 1, hi);
+   }
+}
+
+void generate_sequence(int A[], int n) {
+   int i;
+   for(i = 0; i < n; i++)
+   {
+      A[i] = rand();
+   }
+}
+
 int main(int argc, char ** argv)
 {
    int arr[] = {5, 10, 9, 6, 4, 7, 3, 2, 8, 1};
+   int seq[1000000];
    int i;
+   clock_t begin, end;
+   double time_taken;
 
-   //insertion_sort(arr, 10);
-   //selection_sort(arr, 10);
+   generate_sequence(seq, 1000000);
+
+   begin = clock();
+
+   //insertion_sort(seq, 1000000);
+   //selection_sort(seq, 1000000);
    //bubble_sort(arr, 10);
-   quick_sort(arr, 0, 9);
+   quicker_sort(seq, 0, 999999);
 
-   for(i = 0; i < 10; i++)
-    {
-       printf("%d\n", arr[i]);
-    }
+   end = clock();
+   time_taken = end - begin;
+   time_taken /= CLOCKS_PER_SEC;
+
+   //for(i = 0; i < 1000000; i++)
+   // {
+   //    printf("%d\n", seq[i]);
+   // }
+
+   printf("time_taken = %f\n", time_taken);
 
    return 0;
 }
